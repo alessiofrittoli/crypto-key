17ca727adc92a6f71c470128263c5c0103405772
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-12 18:43:36 +0100

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+export { default as Cipher } from './Cipher'
+export { default as Hash } from './Hash'
 export { default as Hmac } from './Hmac'
+export { default as Scrypt } from './Scrypt'
+
+export * from './utils'
