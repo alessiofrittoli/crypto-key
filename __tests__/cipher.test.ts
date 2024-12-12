5ed81d2d2a39102a281463f2be7364bf95d66cda
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2024-12-12 18:32:15 +0100

    feature: add `Cipher` utility static class (#7)

---
+++ __tests__/cipher.test.ts
@@ -0,0 +1,52 @@
+import Cipher from '@/Cipher'
+import { bufferEquals } from '@alessiofrittoli/crypto-buffer'
+
+const dataToEncrypt	= 'my TOP-SECRET message'
+const password		= 'verystrong-password'
+
+describe( 'Cipher.encrypt()', () => {
+
+	it( 'encrypts data', () => {
+		expect( Cipher.encrypt( dataToEncrypt, password ) )
+			.toBeInstanceOf( Buffer )
+	} )
+
+
+	it( 'encrypts data with custom algorithm', () => {
+		expect( Cipher.encrypt( dataToEncrypt, password, { algorithm: 'aes-192-gcm' } ) )
+			.toBeInstanceOf( Buffer )
+	} )
+
+
+	it( 'encrypts data with custom salt length', () => {
+		expect( Cipher.encrypt( dataToEncrypt, password, { salt: 64 } ).length )
+			.toBe( 117 )
+	} )
+
+
+	it( 'encrypts data with custom salt length', () => {
+		expect( Cipher.encrypt( dataToEncrypt, password, { iv: 32 } ).length )
+			.toBe( 101 )
+	} )
+
+
+	it( 'always produce a unique result', () => {
+		const encrypted1 = Cipher.encrypt( dataToEncrypt, password )
+		const encrypted2 = Cipher.encrypt( dataToEncrypt, password )
+		
+		expect( bufferEquals( encrypted1, encrypted2 ) )
+			.toBe( false )
+	} )
+
+} )
+
+
+describe( 'Cipher.decrypt()', () => {
+
+	it( 'decrypts data', () => {
+		const encrypted = Cipher.encrypt( dataToEncrypt, password )
+		expect( Cipher.decrypt( encrypted, password ).toString() )
+			.toBe( dataToEncrypt )
+	} )
+
+} )
---
+++ src/Cipher.ts
@@ -0,0 +1,137 @@
+import crypto from 'crypto'
+
+
+interface CipherEncryptDecryptOptions
+{
+	/** The Cipher GCM algorithm to use. Default: `aes-256-gcm`. */
+	algorithm?: crypto.CipherGCMTypes
+	/** The salt length. Minimum: `16`, Maximum: `64`. Default: `32`. */
+	salt?: number
+	/** The `IV` length. Minimum: `16`, Maximum: `32`. Default: `16`. */
+	iv?: number
+}
+
+
+class Cipher
+{
+	private static SALT_LENGTH = {
+		min		: 16,
+		max		: 64,
+		default	: 32,
+	}
+
+
+	private static IV_LENGTH = {
+		min		: 16,
+		max		: 32,
+		default	: 16,
+	}
+
+
+	/**
+	 * Encrypt data.
+	 * 
+	 * @param	data	The data to encrypt.
+	 * @param	secret	The secret key used to encrypt the `data`.
+	 * @param	options	( Optional ) Additional options.
+	 * @returns	The encrypted result Buffer.
+	 */
+	static encrypt(
+		data	: crypto.BinaryLike,
+		secret	: crypto.BinaryLike,
+		options	: CipherEncryptDecryptOptions = {},
+	)
+	{
+		options.salt	||= Cipher.SALT_LENGTH.default
+		options.iv	||= Cipher.IV_LENGTH.default
+		options.salt	= Math.min( Math.max( options.salt, Cipher.SALT_LENGTH.min ), Cipher.SALT_LENGTH.max )
+		options.iv	= Math.min( Math.max( options.iv, Cipher.IV_LENGTH.min ), Cipher.IV_LENGTH.max )
+
+		const { keyLength, algorithm } = Cipher.getKeyLength( options.algorithm )
+
+		const salt		= crypto.randomBytes( options.salt )
+		const KEY		= crypto.scryptSync( secret, salt, keyLength )
+		const IV		= crypto.randomBytes( options.iv )
+		const cipher	= crypto.createCipheriv( algorithm, KEY, IV )
+		const encrypted	= Buffer.concat( [ cipher.update( data ), cipher.final() ] )
+		const authTag	= cipher.getAuthTag()
+
+		return Buffer.concat( [ encrypted, IV, authTag, salt ] )
+	}
+
+
+	/**
+	 * Decrypt data.
+	 * 
+	 * @param	data	The data to decrypt.
+	 * @param	secret	The secret key used to decrypt the `data`.
+	 * @param	options	( Optional ) Additional options. Must be the same used while encrypting data.
+	 * @returns	The decrypted data Buffer.
+	 */
+	static decrypt(
+		data	: Buffer,
+		secret	: crypto.BinaryLike,
+		options	: CipherEncryptDecryptOptions = {},
+	)
+	{
+		options.salt	||= Cipher.SALT_LENGTH.default
+		options.iv	||= Cipher.IV_LENGTH.default
+		options.salt	= Math.min( Math.max( options.salt, Cipher.SALT_LENGTH.min ), Cipher.SALT_LENGTH.max )
+		options.iv	= Math.min( Math.max( options.iv, Cipher.IV_LENGTH.min ), Cipher.IV_LENGTH.max )
+
+		const { keyLength, algorithm } = Cipher.getKeyLength( options.algorithm )
+	
+		const salt = data.subarray( options.salt * -1 )
+		data = data.subarray( 0, options.salt * -1 )
+		const authTag = data.subarray( options.iv * -1 )
+		data = data.subarray( 0, options.iv * -1 )
+		const IV = data.subarray( options.iv * -1 )
+		data = data.subarray( 0, options.iv * -1 )
+	
+		const KEY = crypto.scryptSync( secret, salt, keyLength )
+	
+		const decipher = (
+			crypto
+				.createDecipheriv( algorithm, KEY, IV )
+				.setAuthTag( authTag )
+		)
+
+		return (
+			Buffer.concat( [
+				decipher.update( data ),
+				decipher.final()
+			] )
+		)
+	}
+
+
+	/**
+	 * Ensure correct key length based on the given algorithm.
+	 * 
+	 * @param	algorithm The AES algorithm name.
+	 * @returns	An object with validated `algorithm` and `keyLength`.
+	 */
+	private static getKeyLength( algorithm: crypto.CipherGCMTypes = 'aes-256-gcm' )
+	{
+		switch ( algorithm ) {
+			case 'aes-128-gcm':
+				return {
+					algorithm: 'aes-128-gcm',
+					keyLength: 128 / 8, // AES-128 needs a 128 bit (16 bytes)
+				} as const
+			case 'aes-192-gcm':
+				return {
+					algorithm: 'aes-192-gcm',
+					keyLength: 192 / 8, // AES-128 needs a 128 bit (24 bytes)
+				} as const
+			case 'aes-256-gcm':
+			default:
+				return {
+					algorithm: 'aes-256-gcm',
+					keyLength: 256 / 8, // AES-256 needs a 256 bit (32 bytes)
+				} as const
+		}
+	}
+}
+
+export default Cipher
