b925d769384fdaf37a69dd99d138025760658db1
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-12 18:25:25 +0100

    chore: improved `Scrypt` method parameters

--- __tests__/scrypt.test.ts
+++ __tests__/scrypt.test.ts
@@ -11,14 +11,14 @@ const options: ScryptOptions = {
 describe( 'Scrypt.hash()', () => {
 
 	it( 'hashes a string', () => {
-		expect( Scrypt.hash( { key: password } ) )
+		expect( Scrypt.hash( password ) )
 			.toBeTruthy()
 	} )
 
 
 	it( 'always returns a unique hash', () => {
-		const hash1	= Scrypt.hash( { key: password } )
-		const hash2	= Scrypt.hash( { key: password } )
+		const hash1	= Scrypt.hash( password )
+		const hash2	= Scrypt.hash( password )
 
 		expect( hash1 === hash2 )
 			.not.toBe( true )
@@ -26,19 +26,20 @@ describe( 'Scrypt.hash()', () => {
 
 
 	it( 'allows hash length customization', () => {
-		expect( Scrypt.hash( { key: password, length: 16 } ).length )
+		expect( Scrypt.hash( password, { length: 16 } ).length )
 			.toBe( 48 )
 	} )
 
 
 	it( 'allows salt length customization', () => {
-		expect( Scrypt.hash( { key: password, length: 16, saltLength: 16 } ).length )
+		expect( Scrypt.hash( password, { length: 16, saltLength: 16 } ).length )
 			.toBe( 32 )
 	} )
 
 
 	it( 'allows algorithm customization', () => {
-		expect( Scrypt.hash( { key: password, length: 16, saltLength: 16, options } ).length ).toBe( 32 )
+		expect( Scrypt.hash( password, { length: 16, saltLength: 16, options } ).length )
+			.toBe( 32 )
 	} )
 
 } )
@@ -47,17 +48,17 @@ describe( 'Scrypt.hash()', () => {
 describe( 'Scrypt.isValid()', () => {
 
 	it( 'validates a Scrpyt hash', () => {
-		expect( Scrypt.isValid( { key: password, hash: Scrypt.hash( { key: password } ) } ) )
+		expect( Scrypt.isValid( password, Scrypt.hash( password ) ) )
 			.toBe( true )
-		expect( Scrypt.isValid( { key: 'wrong password', hash: Scrypt.hash( { key: password } ) } ) )
+		expect( Scrypt.isValid( 'wrong password', Scrypt.hash( password ) ) )
 			.toBe( false )
 	} )
 
 
 	it( 'validates a Scrpyt hash with custom options', () => {
-		expect( Scrypt.isValid( { key: password, hash: Scrypt.hash( { key: password, options } ), options } ) )
+		expect( Scrypt.isValid( password, Scrypt.hash( password, { options } ), { options } ) )
 			.toBe( true )
-		expect( Scrypt.isValid( { key: 'wrong password', hash: Scrypt.hash( { key: password, options } ), options } ) )
+		expect( Scrypt.isValid( 'wrong password', Scrypt.hash( password, { options } ), { options } ) )
 			.toBe( false )
 	} )
 
