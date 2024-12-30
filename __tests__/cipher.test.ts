17a6b9c2ee463492cea33c41f50b11afaa3aee33
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-30 15:59:02 +0100

    test: add additional cases for custom AES encryption algorithms

--- __tests__/cipher.test.ts
+++ __tests__/cipher.test.ts
@@ -10,10 +10,22 @@ describe( 'Cipher.encrypt()', () => {
 		expect( Cipher.encrypt( dataToEncrypt, password ) )
 			.toBeInstanceOf( Buffer )
 	} )
+	
+	
+	it( 'encrypts data with custom `aes-128-gcm` algorithm', () => {
+		expect( Cipher.encrypt( dataToEncrypt, password, { algorithm: 'aes-128-gcm' } ) )
+			.toBeInstanceOf( Buffer )
+	} )
 
 
-	it( 'encrypts data with custom algorithm', () => {
+	it( 'encrypts data with custom `aes-192-gcm` algorithm', () => {
 		expect( Cipher.encrypt( dataToEncrypt, password, { algorithm: 'aes-192-gcm' } ) )
+			.toBeInstanceOf( Buffer )
+	} )
+
+
+	it( 'encrypts data with custom `aes-256-gcm` algorithm', () => {
+		expect( Cipher.encrypt( dataToEncrypt, password, { algorithm: 'aes-256-gcm' } ) )
 			.toBeInstanceOf( Buffer )
 	} )
 
